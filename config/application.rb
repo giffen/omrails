a6231a7ee12bbc8e7cca2d1cbad2cd2c570f31a8
Andy Giffen <giffen@gmail.com>
2013-03-29 11:12:24 -0700

    Add User Model with Devise

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Onrails
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Required for Devise
+    config.assets.initialize_on_precomplie = false
   end
 end
