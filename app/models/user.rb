789ec0894cd3ac612b36f287130b8622997d47b5
Andy Giffen <giffen@gmail.com>
2013-03-29 17:15:47 -0700

    Added User_ID to user controller as accessible field

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
           :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :user_id
   # attr_accessible :title, :body
 
   has_many :pins
